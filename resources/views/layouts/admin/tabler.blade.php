7b3ffdf9228cd8bd565e308926f10ac4192a32c6
redkaapangestika <redkapgstaa@gmail.com>
2026-07-12 10:27:53 +0700

    chore: ubah page title Tabler template menjadi E-Presensi Kalurahan Condongcatur

--- resources/views/layouts/admin/tabler.blade.php
+++ resources/views/layouts/admin/tabler.blade.php
@@ -13,7 +13,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <title>E-Presensi Kalurahan Condongcatur</title>
     <!-- CSS files -->
     <link href="{{ asset('tabler/dist/css/tabler.min.css?1674944402') }}" rel="stylesheet" />
     <link href="{{ asset('tabler/dist/css/tabler-flags.min.css?1674944402') }}" rel="stylesheet" />
